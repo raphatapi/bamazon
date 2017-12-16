625b1a368b87a1b739be3b4fd4ddd38b35821f1e
raphatapi <raphaeltapioca10@gmail.com>
2017-12-15 22:32:39 -0700

    update quantity

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,5 +1,6 @@
 var inquirer = require("inquirer");
 var mysql = require("mysql");
+var columnify = require('columnify');
 
 var connection = mysql.createConnection({
     host: "localhost",
@@ -15,57 +16,95 @@ connection.connect(function(err){
 });
 
 function start() {
-    connection.query("SELECT item_id, product_name, price FROM products", function(err, res){
+    connection.query("SELECT item_id, product_name, price, stock_quantity FROM products", function(err, res){
         if (err) throw err;
-        console.log("Items for sale!\n");
-        for (var i = 0; i < res.length; i++){
-            console.log(
-                "ID: " +
-                res[i].item_id +
-                " || Item: " +
-                res[i].product_name +
-                " || Price: $" +
-                res[i].price
-             );
+        console.log("*****************");
+        console.log("*ITEMS FOR SALE!*");
+        console.log("*****************");
+        for (var i = 0; i < res.length; i++) {
+            var columns = columnify([{
+                ID: res[i].item_id,
+                NAME: res[i].product_name,
+                PRICE: "$" + res[i].price,
+                QUANTITY: res[i].stock_quantity
+              }],{
+                showHeaders: i === 0,
+                columnSplitter: ' | ',
+                minWidth: 30,
+                config: {
+                  description: {maxWidth: 40}
+                }
+              })
+            console.log(columns);
         };
+        whatItem();
     });
-    purchase();
 };
 
-function purchase() {
-    connection.query("SELECT * FROM products", function(err, results){
-        if (err) throw err;
-        inquirer.prompt([
-        {
-            name: "item",
-            type: "input",
-            message: "What item would you like to purchase? (Choose ID)",
-            validate: function(value) {
-                if (isNaN(value) === false) {
-                  return true;
+function whatItem() {
+    inquirer.prompt([
+    {
+        name: "item",
+        type: "input",
+        message: "What item would you like to purchase? (Choose ID)",
+        validate: function(value) {
+            if (isNaN(value) === false) {                    
+                return true;
                 }
                 return false;
-              }
-        },
+            }
+    }
+    ]).then(function(answer) {
+        var query = "SELECT * FROM products WHERE ?";
+        connection.query(query, { item_id: answer.item }, function(err, res) {
+            if (err) throw err;
+            if (res.length === 0) {
+                console.log("\nUnfortunately this item isn't available. Please choose an item listed above.\n");
+                whatItem();
+                return;
+            } 
+            howMany(res[0].stock_quantity, answer.item, res[0].price);
+            return;
+        });
+    });
+};
+
+function howMany(quantity, itemId, price) {
+    inquirer.prompt([
         {
             name: "quantity",
             type: "input",
             message: "How many of this would you like to buy?",
             validate: function(value) {
                 if (isNaN(value) === false) {
-                  return true;
+                    return true;
                 }
                 return false;
             }
         }
-        ]).then(function(answer){
-            var chosenItem;
-            for (var i = 0; i < results.length; i++) {
-              if (results[i].item_id === answer.item) {
-                chosenItem = results[i];
-              }
-            }
-        })
-    })
-    
-}
+    ]).then(function(answer){
+        if (answer.quantity > quantity) {
+            console.log("\nI can only give you " + quantity.toString() + ". Please enter a valid quantity.\n");
+            howMany(quantity);
+            return;
+        } else if (answer.quantity == quantity) {
+            console.log("\nI can give you " + answer.quantity.toString() + ". This all I have!\n");
+        } else {
+            console.log("\nItems placed in your cart.\n");
+        }
+        var query1 = "UPDATE products SET ? WHERE ?";
+        connection.query(query1,
+            [
+                {
+                    stock_quantity: quantity - answer.quantity
+                },
+                {
+                    item_id: itemId
+                }
+            ], function(err, res) {
+                if (err) throw err;
+                console.log("\nYour total is $" + (answer.quantity * price).toString() + "\n");
+                setTimeout(start, 2000);
+        });
+    });
+};
